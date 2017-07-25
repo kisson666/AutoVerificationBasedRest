24b7ffd20f13837caa54686dac6c49cdf608c55d
VimT <562593188@qq.com>
2017-07-25 22:30:50 +0800

    [rewrite] use BaseHttpRequestHandler

--- README.md
+++ README.md
@@ -1,18 +1,30 @@
-# 基于rest接口的自动化验证软件
+# 基于REST接口的自动化验证软件
 
-## 依赖
-- flask
-- xlrd
-
-`pip install flask xlrd`
+## 环境
+Python 2.7版本，不需要任何依赖
 
 ## 使用
-`python auto.py [-h] [-p PORT] filepath`
+    python auto.py [-h] [-p PORT] [-m METHOD] url response
 
-## 文件格式
-支持json和excel两种文件格式  
-json的格式默认为postman的导出格式  
-excel 要求必须要有 name url method response 这四列，且必须有列头  
+其中`-h` `-p` `-m` `--host` 为可选参数
 
-可以参考示例文件[book.postman_collection.json](https://github.com/VimT/AutoVerificationBasedRest/blob/master/book.postman_collection.json)和[book.xlsx](https://github.com/VimT/AutoVerificationBasedRest/blob/master/book.xlsx)
+    -h, --help 显示帮助信息
+    --host  监听地址
+    -p PORT, --port PORT  监听端口
+    -m METHOD, --method METHOD 指定API方法，默认为GET
 
+`url` `response` 为必选参数
+
+    url 为api的url地址
+    response 为返回值，如果有返回值有空格，请用引号将返回值内容括起来
+
+
+## 举例
+
+```bash
+python auto.py /user/login 登录成功 # url 和 response内容必须要有
+python auto.py /user/login {\"code\":0}  #没有空格，但是正常的引号要用 \" 防止被解析
+python auto.py /abc "如果中间有空格 要用引号括起来"
+python --host 0.0.0.0 -p 80 -m POST /支持中文路径 返回测试  #监听所有网卡地址，端口为80，方法为POST
+
+```
--- auto.py
+++ auto.py
@@ -1,121 +1,89 @@
+#!/usr/bin/env python
 # coding=utf-8
 
-"""
-### 思路：
-- 从文件读取信息，文件是json格式的？兼容POSTMAN？文件是excel的？或者做个web页面？直接在web页面上改很方便啊
-- 支持各种method，参数该怎么处理，post参数的参数怎么实现？
-- 如何与静态文件共存？
-- 如何兼容auto refresh？
-- 对后台的自动化测试
-- 实现单文件
-"""
 import argparse
-import json
-import urlparse
+import socket
+import sys
+from BaseHTTPServer import BaseHTTPRequestHandler
 
-import xlrd
-from flask import Flask, make_response
-
-app = Flask(__name__)
+from urllib import quote
 
 
-class Api(object):
-    """ This is a api class, resolve xlsx or json to Api class
-    """
+class RouteHandler(BaseHTTPRequestHandler):
+    method = None
+    url = None
+    response = None
 
-    def __init__(self, name, url, method, response, param):
-        self.name = name
-        self.url = url
-        self.method = method
-        self.response = response
-        self.param = param
+    def handle_api(self):
+        if self.command != self.method:
+            self.send_error(501, "Unsupported method (%r)" % self.command)
+            return
+        if self.path != self.url:
+            self.send_error(404, "File not found.")
+            return
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        self.wfile.write(self.response.encode("utf-8"))
 
+    def handle_one_request(self):
+        """Handle a single HTTP request.
 
-def parse_json_api(filepath):
-    with open(filepath, 'r') as f:
-        content = f.read()
-    raw_api = json.loads(content)
-    apis = []
-    for i in raw_api['item']:
+        You normally don't need to override this method; see the class
+        __doc__ string for information on how to handle specific HTTP
+        commands such as GET and POST.
+
+        """
         try:
-            formdata = [j['key'] for j in i['request']['body']['formdata']]
-        except KeyError as e:
-            formdata = []
-        url = i['request']['url']
-        if url.startswith("http"):
-            rs = urlparse.urlparse(url)
-            url = rs.path
-
-        api = Api(name=i['name'], url=url, method=i['request']['method'], response=i['response'],
-                  param=formdata)
-        apis.append(api)
-    return apis
-
-
-def parse_xls_api(filepath):
-    workbook = xlrd.open_workbook(filepath)
-    sheet = workbook.sheets()[0]
-    nrows = sheet.nrows
-    ncols = sheet.ncols
-    head = sheet.row_values(0)
-    refer = dict(zip(head, range(len(head))))
-    result = []
-    for i in range(1, nrows):
-        row = sheet.row_values(i)
-        param = row[refer['param']] if refer.get('param') else None
-        try:
-            api = Api(row[refer['name']], row[refer['url']], row[refer['method']], row[refer['response']], param)
-            result.append(api)
-        except KeyError as e:
-            print "缺少参数" + str(e)
-            exit(1)
-    return result
+            self.raw_requestline = self.rfile.readline(65537)
+            if len(self.raw_requestline) > 65536:
+                self.requestline = ''
+                self.request_version = ''
+                self.command = ''
+                self.send_error(414)
+                return
+            if not self.raw_requestline:
+                self.close_connection = 1
+                return
+            if not self.parse_request():
+                # An error code has been sent, just exit
+                return
+            self.handle_api()
+            self.wfile.flush()  # actually send the response if not already done.
+        except socket.timeout, e:
+            # a read or a write timed out.  Discard this connection
+            self.log_error("Request timed out: %r", e)
+            self.close_connection = 1
+            return
 
 
-def import_file(filepath):
-    """import files"""
-    suffix = None
-    allows = ['xls', 'xlsx', 'json']
-    if "." in filepath:
-        suffix = filepath.rsplit(".", 1)[-1].lower()
-        if suffix not in allows:
-            print "不正确的文件格式，允许" + ", ".join(allows)
-            exit(1)
-    else:
-        print "没有文件后缀名，允许" + ", ".join(allows)
-        exit(1)
-    if suffix == "json":
-        return parse_json_api(filepath)
-    else:
-        return parse_xls_api(filepath)
+def run(host, port, url, method, response):
+    # Start a simple server, and loop forever
+    from BaseHTTPServer import HTTPServer
+    RouteHandler.url = quote(url.encode("utf-8"))
+    RouteHandler.response = unicode(response)
+    RouteHandler.method = method.upper()
+
+    server = HTTPServer((host, port), RouteHandler)
+    print "The following api is running, use <Ctrl-C> to stop"
+    print method.upper() + " http://" + host + ":" + str(port) + quote(url.encode("utf-8"))
+    server.serve_forever()
 
 
-def add_route(api):
-    """add view route by api"""
-
-    def generate_view_func(response_value):
-        def warp():
-            response = make_response(response_value)
-            response.headers['Access-Control-Allow-Origin'] = '*'
-            return response
-
-        return warp
-
-    app.add_url_rule(api.url, api.name, generate_view_func(api.response), methods=[api.method])
-    print u"add API %10s %5s %-30s" % (api.name, api.method.upper(), api.url)
-
-
-def main(port, filepath):
-    apis = import_file(filepath)
-    if len(apis) > 0:
-        for i in apis:
-            add_route(i)
-    app.run(port=port)
-
-
-if __name__ == "__main__":
+def main():
     parser = argparse.ArgumentParser(description="Auto verification based rest")
-    parser.add_argument('filepath', help='the path to api file')
-    parser.add_argument('-p', "--port", type=int, default=3000, help='the port of server')
+    parser.add_argument('url', type=lambda s: s.decode(sys.getfilesystemencoding()), help=u"url地址，例如 /user/login ")
+    parser.add_argument('response', type=lambda s: s.decode(sys.getfilesystemencoding()),
+                        help=u'返回值，如果有返回值有空格，请用引号将返回值内容括起来')
+    parser.add_argument("--host", default="localhost", help=u'监听地址')
+    parser.add_argument('-p', "--port", type=int, default=3000, help=u'监听端口')
+    parser.add_argument('-m', '--method', default="GET", help=u"指定HTTP方法，默认为GET")
     args = parser.parse_args()
-    main(args.port, args.filepath)
+    try:
+        run(args.host, args.port, args.url, args.method, args.response)
+    except KeyboardInterrupt as e:
+        print u"结束运行"
+
+
+if __name__ == '__main__':
+    main()
